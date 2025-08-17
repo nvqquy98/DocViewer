64dcdaa8ffdf16b230a969474e347554e50953a8
nvqquy98 <nvqquy98@gmail.com>
2025-08-17 23:54:45 +0700

    public class PdfDownloader

--- library/src/main/java/com/nvqquy98/lib/doc/pdf/PdfDownloader.kt
+++ library/src/main/java/com/nvqquy98/lib/doc/pdf/PdfDownloader.kt
@@ -24,7 +24,7 @@ import javax.net.ssl.HttpsURLConnection
  */
 const val OK_HOSTNAME_VERIFIER_CLASS = "com.android.okhttp.internal.tls.OkHostnameVerifier";
 
-internal class PdfDownloader(url: String, private val listener: OnDownloadListener) {
+open class PdfDownloader(url: String, private val listener: OnDownloadListener) {
 
     init {
         listener.getCoroutineScope().launch(Dispatchers.IO) { download(url) }
